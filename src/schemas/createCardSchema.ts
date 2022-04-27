37c41011e8f1f4003a4f8e121940d79c97eb6ff3
Marcus <marcusmoraes2010@hotmail.com>
2022-04-27 19:45:24 -0300

    Preparando revisão

--- src/schemas/createCardSchema.ts
+++
@@ -1,8 +0,0 @@
-import joi from 'joi';
-
-const createCardSchema = joi.object({
-    employeeId: joi.number().required(),
-    cardType: joi.string().required().valid('groceries', 'restaurant', 'transport', 'education', 'health').insensitive()
-})
-
-export default createCardSchema;
--- src/schemas/purchaseSchema.ts
+++
@@ -1,9 +0,0 @@
-import joi from 'joi';
-
-const purchaseSchema = joi.object({
-    password: joi.string().required(),
-    businessId: joi.number().required(),
-    price: joi.number().required()
-})
-
-export default purchaseSchema;
